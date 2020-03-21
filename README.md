8eb9661532f79f19b3ecd28b3bd85aa99ec51b06
janakhpon <jnovaxer@gmail.com>
2020-03-21 21:22:11 +0630

    GDEV000 just finished configuring component structures

--- README.md
+++
@@ -1,2 +0,0 @@
-# Reusable-D3-Components
-Reusable D3 Components with material-ui to use in React Application
---
+++ src/theme.js
@@ -0,0 +1,28 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import { red } from '@material-ui/core/colors';
+
+// A custom theme for this app
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#2f3542',
+    },
+    secondary: {
+      main: '#00171f',
+    },
+    text: {
+      primary: "#000",
+      secondary: "#2f3542"
+    },
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: '#8395a7',
+    },
+  },
+
+});
+
+
+export default theme;
